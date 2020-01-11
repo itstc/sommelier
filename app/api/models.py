af25330897d7be2c219e25aa2b574ed898c2352e
itstc <itsthomaschu@gmail.com>
2020-01-11 15:47:41 -0700

    create Wine model

--- app/api/models.py
+++ app/api/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Wine(models.Model):
+    name=models.CharField(max_length=255)
+    description=models.TextField()
+    points=models.IntegerField(blank=False, default=0)
+    price=models.DecimalField(blank=False, default=0, max_digits=999, decimal_places=2)
