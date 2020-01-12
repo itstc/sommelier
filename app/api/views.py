6ee67ccf893608ef25b560fa1890186820d4229f
Echo-Gambino <nkb012pomm@gmail.ca>
2020-01-12 00:11:07 +0000

    Made some changes to make the django web app to work properly

--- app/api/views.py
+++ app/api/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from django.http import JsonResponse
+import json
+from .models import Wine
 
 # Create your views here.
+@api_view(["GET"])
+def wines(request):
+
+    data = {
+        "wines": [
+            'best wine',
+            "okay wine",
+            'bad wine'
+        ]
+    }
+
+    return JsonResponse(data)
+    
+
